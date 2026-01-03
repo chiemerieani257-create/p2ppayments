85c4f84c6a5259c26e2b89facd416ec5a5943600
chiemerieani257-create <chiemerieani257@gmail.com>
2026-01-03 22:04:53 +0100

    Create Home component with main layout and logo

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#ffffff"
+    }}>
+      <img
+        src="/pi-logo.png"
+        alt="Pi Network"
+        width="220"
+        height="220"
+      />
+    </main>
+  );
+}
